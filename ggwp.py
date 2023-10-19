3258355d13d6e1b005c46b20208917c75bf18e6b
xirfk24 <rsanusi24@gmail.com>
2023-10-19 19:58:31 +0700

    variabel type data 2

---
+++ ggwp.py
@@ -0,0 +1,11 @@
+#proses memasukan data ke dalam variabel
+nama = "John Doe"
+#proses mencetak variabel
+print(nama)
+#nilai dan tipe data dalam variabel dapat diubah
+umur = 21       #nilai awal
+print(umur)     #mencetak nilai umur
+type(umur)      #mengecek tipe data umur
+umur = "dua puluh satu"     #nilai setelah diubah
+print(umur)     #mencetak nilai umur
+type(umur)      #mengecek tipe data umur
