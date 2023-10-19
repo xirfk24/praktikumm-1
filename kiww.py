297d4df093279eac0de97855e7f2b14fef941675
xirfk24 <rsanusi24@gmail.com>
2023-10-19 19:43:42 +0700

    variabel type data

---
+++ kiww.py
@@ -0,0 +1,15 @@
+namaDepan = "Ireng"
+namaBelakang = "Susanto"
+nama = namaDepan + "" + namaBelakang
+umur = 22
+hobi = "Berenang"
+print ("Biodata", nama, "", umur, "", hobi)
+#contoh varibel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+ini_variabel_juga = "Hi"
+ini_variabel_22 = "Bye"
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
